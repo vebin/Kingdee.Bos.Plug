558c66efc40e330ba7b52c0acade3ef2fad40528
catdemon <wx@mgitd.com>
2020-08-20 22:34:58 +0800

    临时提交

--- Source/MgSoft.K3Cloud.WebApi/Model/BaseData.cs
+++ Source/MgSoft.K3Cloud.WebApi/Model/BaseData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +7,12 @@ namespace MgSoft.K3Cloud.WebApi.Model
 {
     public class BaseData
     {
-        public long Id { get; set; }
+        public object Id { get; set; }
 
         public string Number { get; set; }
 
         public MultiLanguageText[] MultiLanguageText { get; set; }
 
-        public List<KeyValuePair<string,string>> Name { get; set; }
+        public List<KeyValuePair<string, string>> Name { get; set; }
     }
 }
--- Source/MgSoft.K3Cloud.WebApi/Model/MultiLanguageText.cs
+++ Source/MgSoft.K3Cloud.WebApi/Model/MultiLanguageText.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,21 @@ namespace MgSoft.K3Cloud.WebApi.Model
     {
         public int LocaleId { get; set; }
         public string Name { get; set; }
+
+        public string FDataValue { get; set; }
+
         public string Description { get; set; }
+
+
+        /// <summary>
+        /// 规格型号
+        /// </summary>
+        public string Specification { get; set; }
+
+        /// <summary>
+        /// 品材/材质
+        /// </summary>
+        [JsonProperty(PropertyName = "F_jghy_MulLangText")]
+        public string Quality { get; set; }
     }
 }
