24e340a8d0c59164563d16515570f5d6df1c2934
凹僧 <1329789050@qq.com>
2020-12-16 10:41:29 +0800

    简单生产入库单Api 添加了仓位Model(StockLocModel.cs)

---
+++ Source/MgSoft.K3Cloud.WebApi/Common/Dto/Sp/Sp_Instock_SerialSubEntity.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MgSoft.K3Cloud.WebApi.Common.Dto.Sp
+{
+    /// <summary>
+    /// (简单生产入库单)
+    /// </summary>
+    public class Sp_Instock_SerialSubEntity
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual long? FDetailID { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual string FSerialNo { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual string FSerialNote { get; set; }
+    }
+}
---
+++ Source/MgSoft.K3Cloud.WebApi/Common/Model/StockLocModel.cs
@@ -0,0 +1,25 @@
+﻿using MgSoft.K3Cloud.WebApi.Common.Dto.Property;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MgSoft.K3Cloud.WebApi.Common.Model
+{
+    /// <summary>
+    /// 仓位
+    /// </summary>
+    public class StockLocModel
+    {
+        /// <summary>
+        /// 货架编码
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual FNumberProperty FSTOCKLOCID__FF100001 { get; set; }
+        /// <summary>
+        /// 位置号编码
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public virtual FNumberProperty FSTOCKLOCID__FF100002 { get; set; }
+    }
+}
