80f25729131c1907eca7f5f505558ec7500b7dc0
凹僧 <1329789050@qq.com>
2021-01-23 11:53:34 +0800

    添加字段

--- Source/MgSoft.K3Cloud.WebApi/Common/Dto/Ap/Ap_PayableGetListOutputDto.cs
+++ Source/MgSoft.K3Cloud.WebApi/Common/Dto/Ap/Ap_PayableGetListOutputDto.cs
@@ -12,9 +12,24 @@ namespace MgSoft.K3Cloud.WebApi.Common.Dto.Ap
     public class Ap_PayableGetListOutputDto : GetListOutputDto
     {
         /// <summary>
+        /// 单据类型
+        /// </summary>
+        [JsonProperty("FBillTypeID")]
+        public string FBillTypeID { get; set; }
+        /// <summary>
+        /// 单据状态
+        /// </summary>
+        [JsonProperty("FDocumentStatus")]
+        public string FDocumentStatus { get; set; }
+        /// <summary>
         /// 明细分录内码
         /// </summary>
         [JsonProperty("FEntityDetail_FEntryId")]
         public long FEntryId { get; set; }
+        /// <summary>
+        /// 含税单价
+        /// </summary>
+        [JsonProperty("FTaxPrice")]
+        public decimal FTaxPrice { get; set; }
     }
 }
